d66858cb61645b526fc6be7aa6ce7edaa3081dfd
Marshall Yount <marshall@codeclimate.com>
2015-01-04 21:14:40 -0600

    binstubs, the rails 4 way

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,4 @@
 gem install bundler
-bundle install --binstubs
+bundle install
+bundle config --delete bin    # Turn off Bundler's stub generator
+rake rails:update:bin         # Use the new Rails 4 executables
