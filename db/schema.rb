25243f09cc6cc3f09d0e14363ea579f0d03725a7
Marshall Yount <marshall@codeclimate.com>
2015-01-07 04:16:59 -0600

    rails generate model Account name campaign_source promo_code remote_ip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105025511) do
+ActiveRecord::Schema.define(version: 20150107101600) do
+
+  create_table "accounts", force: true do |t|
+    t.string   "name"
+    t.string   "campaign_source"
+    t.string   "promo_code"
+    t.string   "remote_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
