8cec5ad22beb61ec127afe7f4024993c1cbd4d34
Marshall Yount <marshall@codeclimate.com>
2015-01-07 07:15:17 -0600

    rails generate migration AddAccountIdToUser

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107101600) do
+ActiveRecord::Schema.define(version: 20150107131424) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150107101600) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "account_id"
   end
 
 end
