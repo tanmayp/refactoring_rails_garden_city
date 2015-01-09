c1faeaea20d960a9a27f7d50d0024f6be9348dce
Marshall Yount <marshall@codeclimate.com>
2015-01-10 00:47:06 +0530

    add NullAccount object

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ActiveRecord::Base
   has_many :users
+
+  def self.for_user(user)
+    user.account || NullAccount.new
+  end
 end
---
+++ app/models/null_account.rb
@@ -0,0 +1,5 @@
+class NullAccount
+  def name
+    "Default Value for Account.name"
+  end
+end
